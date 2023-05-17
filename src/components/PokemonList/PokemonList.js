24c8c0121ac21deecb3ff35c7efcae2ca614e71e
Corentin <100130817+CorentinEsteve@users.noreply.github.com>
2023-05-17 11:21:38 +0200

    pokemon list displayed ✨

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,12 +1,34 @@
-import React  from "react";
+import React from "react";
+import axios from "axios";
 
 function PokemonList({ searchTerm = "" }) {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=100`;
 
+  const [list, setList] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get(url)
+      .then((response) => {
+        setList(response.data.results);
+        console.log(response.data.results)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [url]);
 
   return (
     <ul>
-    <li>test</li>
-    <li>test2</li>
+      {list
+        .filter((pokemon) => {
+          return pokemon.name.includes(searchTerm);
+        })
+        .map((pokemon) => (
+          <>
+            <li key={pokemon.name}>{pokemon.name}</li>
+            <img key={pokemon.url} src={pokemon.url} alt={pokemon.name} />
+          </>
+        ))}
     </ul>
   );
 }
